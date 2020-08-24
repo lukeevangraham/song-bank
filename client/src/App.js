bfd1ddf21f14cf986c16f791c5c4ad0a8b3bf89f
Cefalu Wave <luke.evan.graham@gmail.com>
2020-08-24 08:48:12 -0700

    nav in

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Navbar } from './components/Navbar'
 import Home from "./components/pages/Home";
 import Submit from "./components/pages/Submit";
 import "./App.css";
@@ -8,10 +9,8 @@ class App extends Component {
   render() {
     return (
       <Router>
+        <Navbar />
         <div>
-          <div className="App-header">
-            <h2>Welcome to SongBank</h2>
-          </div>
           <Route exact path="/" component={Home} />
           <Route exact path="/submit" component={Submit} />
         </div>
